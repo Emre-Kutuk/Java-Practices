067c32cca3c7a64ae87f89177b21ce978fd0f0b0
Emre <kutukemre68@gmail.com>
2020-08-02 01:35:00 +0200

    Small Changes

---
+++ TestProject/src/Program.java
@@ -0,0 +1,13 @@
+public class Program {
+
+    public static void main(String[] args)
+    {
+        Program myprogram = new Program();
+        myprogram.Start();
+    }
+
+    void Start()
+    {
+        System.out.print("This is a test");
+    }
+}
